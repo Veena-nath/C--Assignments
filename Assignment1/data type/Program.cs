abda907a0b46a21f18223d18ed54894c628512f2
Veena-nath <jrveena01@gmail.com>
2025-05-15 04:52:05 -0700

    Add files via upload

---
+++ Assignment1/data type/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace data_type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //get the base of triangle
+            Console.WriteLine("Enter the base of triangle:");
+            int Base = Convert.ToInt32(Console.ReadLine());
+            // get the height of triangle
+            Console.WriteLine("Enter the height of triangle:");
+            int Height = Convert.ToInt32(Console.ReadLine());
+            // get the slope of triangle
+            Console.WriteLine("Enter the slope of the triangle:");
+            int slope = Convert.ToInt32(Console.ReadLine());
+            //Calclucate the area of the triangle
+            double area = (Base * Height) / 2;
+            //Calclucate the perimeter of th etriangle
+            int perimeter = Base + Height + slope;
+            //Display the result
+            Console.WriteLine($"Area of the triangle is:{area}");
+            Console.WriteLine($"Perimeter of the triangle is:{perimeter}");
+
+        }
+    }
+}
---
+++ Assignment1/data type1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace data_type1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.Write("Enter the distance:");
+            int distance = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the time:");
+            int time = Convert.ToInt32(Console.ReadLine());
+            int speed = distance / time;
+            Console.WriteLine($"The speed in killometer per hour is:{speed}");
+            Console.WriteLine($"The speed in miles per hour is{speed}");
+        }
+    }
+}
