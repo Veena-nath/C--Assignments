6f138f3c199aac0603c8bec1992dda2e124b502c
Veena-nath <jrveena01@gmail.com>
2025-05-25 21:39:40 -0700

    Add files via upload

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //get input
+            Console.WriteLine("Enter the string");
+            string input=Console.ReadLine();
+            //reverse the string
+            Console.WriteLine("Character in reverse Order");
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(input[i]);
+            }
+
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string:");
+            string input=Console.ReadLine();
+
+                //length of character in string
+                Console.WriteLine($"Total number of character: { input.Length}");
+
+        }
+    }
+}
